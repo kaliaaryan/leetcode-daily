516ccd889623bd4d7a76c3ee19d951604a448f02
kalia aryan <kalia.aryan17@gmail.com>
2023-10-31 15:43:58 +0530

    Solution to the daily LeetCode question for 31/10/2023

--- find_the_original_array_of_prefix_xor.cpp
+++ find_the_original_array_of_prefix_xor.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans(n);
         ans[0] = pref[0];
         for(int i=1; i<n; i++){
-            ans[i] = pref[i]^pref[i-1];
+            ans[i] = pref[i] ^ pref[i-1];
         }
         return ans;
     }
