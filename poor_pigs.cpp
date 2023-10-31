dd1d896f1a66d50e34ea1296766fa9206ad25228
kalia aryan <kalia.aryan17@gmail.com>
2023-10-31 15:39:40 +0530

    Solution to the daily LeetCode question for 29/10/2023

--- poor_pigs.cpp
+++ poor_pigs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        return ceil(log2(buckets)/log2(int(minutesToTest/minutesToDie)+1));
+        return ceil(log2(buckets)/log2(int(minutesToTest/minutesToDie) + 1));
     }
 };
