f3b2ba902b0fcdd23060907ffb2965e7f60ac774
kalia aryan <kalia.aryan17@gmail.com>
2023-11-04 22:56:24 +0530

    Solution to the daily LeetCode question for 04/11/2023

--- last_moments_before_all_ants_fall_out_of_planck.cpp
+++ last_moments_before_all_ants_fall_out_of_planck.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         int ans=0;
-        for(int i=0;i<left.size();i++) // you can also use for-each loop like " for(int i : left) "
+        for(int i=0;i<left.size();i++) 
             ans=max(ans,(abs(0-left[i])));  
         for(int i=0;i<right.size();i++)
             ans=max(ans,abs(n-right[i]));        
